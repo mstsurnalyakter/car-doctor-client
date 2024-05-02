56e01d0477d0e33e1abf351ff696b57bfcf52c45
mstsurnalyakter <surnalyms@gmail.com>
2024-05-02 12:33:01 +0600

    firebase install and setup

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCSS_ogDRfackRuaFxb64Xl4z3EClwUn8A",
-  authDomain: "cars-doctor-client-42986.firebaseapp.com",
-  projectId: "cars-doctor-client-42986",
-  storageBucket: "cars-doctor-client-42986.appspot.com",
-  messagingSenderId: "156777642298",
-  appId: "1:156777642298:web:bea910f50bb811abce7803",
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default auth;
